4f7e3983be58e7ca41c2067292e09c61bfbf48e6
Рома <onlybrinty@gmail.com>
2023-02-03 17:13:42 +0300

    first sketches

---
+++ interface.py
@@ -0,0 +1,50 @@
+import pygame
+pygame.init()
+
+BUTTON_TEXT_COLOR = (247, 239, 174)
+LABEL_TEXT_COLOR = 'white'
+
+
+class Texture:
+    def __init__(self, position, image):
+        self.image = image
+        self.rect = self.image.get_rect(center=position)
+
+
+class Button:
+    def __init__(self, position, image, text):
+        self.text = text
+        self.image = image
+        self.rect = self.image.get_rect(topleft=position)  # Прямоугольник картинки
+
+        # Настройка шрифтов и размеров
+        font = int(self.rect.width // len(self.text) * 1.5)
+        font = pygame.font.Font('assets/pixeboy.ttf', font)
+        self.label = font.render(self.text, True, BUTTON_TEXT_COLOR)
+
+        label_width, label_height = font.size(self.text)  # Размеры текста
+        # Центрирование текста в кнопке
+        self.label_x = self.rect.x + (self.rect.width - label_width) // 2
+        self.label_y = self.rect.y + (self.rect.height - label_height) // 2
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+        screen.blit(self.label, (self.label_x, self.label_y))
+
+    def __call__(self):
+        return self.text
+
+
+class Label(Texture):   # класс для надписи
+    def __init__(self, position, text, font):
+        self.font = pygame.font.Font('assets/pixeboy.ttf', font)
+        self.pos = position
+
+        self.change_text(text)
+
+    def change_text(self, new_text):
+        self.text = new_text
+        super().__init__(self.pos, self.font.render(new_text, True, LABEL_TEXT_COLOR))
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
---
+++ main.py
@@ -0,0 +1,42 @@
+from interface import Label, Button
+import pygame
+
+BUTTON_IMAGE = pygame.image.load('button.png')
+WIDTH, HEIGHT = 1280, 720
+FPS = 60
+
+
+class App:
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT), vsync=True)
+        self.map = Map()
+
+        some_button = Button((100, 285), BUTTON_IMAGE, 'button')
+        search_text_label = Label((100, 75), 'Some address', 70)
+        self.interface = (some_button, search_text_label)
+
+        clock = pygame.time.Clock()
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:  # сохраняем игру перед выходом
+                    pygame.quit()
+                    exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 2:
+                        pass
+                if event.type == pygame.MOUSEWHEEL:
+                    self.delta = str(min(4, max(1.5, float(self.map.delta) + event.y / 100)))
+
+            self.draw()
+            clock.tick(FPS)
+
+    def draw(self):
+        self.screen.blit(self.map.get_image(), (0, 0))
+
+        for element in self.interface:  # рисуем интерфейс
+            element.draw(self.screen)
+
+        pygame.display.update()
