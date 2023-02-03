36967e2ab18a60416871fb10afa9d88b09a586a5
Рома <onlybrinty@gmail.com>
2023-02-03 17:20:56 +0300

    working app

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ class App:
         search_text_label = Label((500, 75), 'Some address', 70)
         self.interface = (some_button, search_text_label)
 
+        self.buttons = (some_button,)
+        self.labels = (search_text_label,)
+
         clock = pygame.time.Clock()
 
         while True:
@@ -25,18 +28,29 @@ class App:
                     pygame.quit()
                     exit()
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == 2:
+                    if event.button == 1:
+                        for button in self.buttons:
+                            if button.rect.collidepoint(event.pos):  # если мышка над кнопкой
+                                self.action(button())  # делаем что-то в action
+                    elif event.button == 2:
                         pass
-                if event.type == pygame.MOUSEWHEEL:
+                elif event.type == pygame.MOUSEWHEEL:
                     self.delta = str(min(4, max(1.5, float(self.map.delta) + event.y / 100)))
+                elif event.type == pygame.KEYDOWN:
+                    if event.unicode:
+                        pass
+
 
             self.draw()
             clock.tick(FPS)
 
+    def action(self, button):
+        pass
+
     def draw(self):
         # self.screen.blit(self.map.get_image(), (0, 0))
 
-        for element in self.interface:  # рисуем интерфейс
+        for element in self.buttons + self.labels:  # рисуем интерфейс
             element.draw(self.screen)
 
         pygame.display.update()
